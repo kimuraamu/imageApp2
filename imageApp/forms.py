a37c71bf37b41cdbd0f08701cfa0be4eed80bddc
木村　洸太 <kimurakota@Ragdoll.local>
2020-12-23 03:15:28 +0900

    Add second

--- imageApp/forms.py
+++ imageApp/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from .models import Document
+from PIL import Image
  
  
-class DocumentForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ('description', 'photo',)
+class DocumentForm(forms.Form):
+    photo = forms.ImageField(required=True) #required=Trueで空の送信を防ぐ
+
+
--- imageApp/views.py
+++ imageApp/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render, redirect
 from .models import Document
 from django import forms
 from .forms import DocumentForm
- 
+from PIL import Image
  
 def index(request):
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    else:
-        form = DocumentForm()
-        obj = Document.objects.all()
- 
-    return render(request, 'imageApp/model_form_upload.html', {
-        'form': form,
-        'obj': obj
-    })
+        if request.method != 'POST':
+            form = DocumentForm()
+            #obj = Document.objects.latest("uploaded_at")
+        else:
+            form = DocumentForm(request.POST, request.FILES)
+            if form.is_valid():
+                photo = form.cleaned_data['photo']
+                document = Document()
+                document.photo = photo
+                document.save()
+                obj = Document.objects.latest("uploaded_at")
+        
+        context = {
+            'form': form,
+            'obj': obj
+        }
+        return render(request, 'imageApp/model_form_upload.html', context)
